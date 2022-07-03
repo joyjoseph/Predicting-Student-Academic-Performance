e470c432db41bca380edad6075e0d541f57e0672
Joy Joseph <joyjoseph9027@gmail.com>
2022-07-03 18:30:59 +0100

    updated change

--- Predict.py
+++ Predict.py
@@ -2,5 +2,5 @@ import joblib
 mj = joblib.load("model_joblib.pkl")
 
 # using the model object mj to make prediction
-project = mj.predict([[2, 9130, 1, 3, 136.0, 1, 3, 38, 119.8, 1, 1, 0, 18, 10, 12, 11.571429, 0, 6, 12.142857]])
+project = mj.predict([[2, 8, 1, 0, 130.0, 4, 5, 5, 130.0, 1, 1, 0, 18, 0, 11.666667, 0, 11.333333]])
 print (project)
